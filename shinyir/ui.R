df61c1a7dcd10e6a1eff0af8f580f9e27746fdf4
Carter Vereschagin <carter.vereschagin@gmail.com>
2026-03-01 19:37:20 -0700

    added in Brandon's ui.R

--- shinyir/ui.R
+++ shinyir/ui.R
@@ -1,26 +1,3 @@
-# library(shiny)
-# library(shinyjs)
-# library(bslib)
-# 
-# 
-# ui <- page_navbar(
-#   useShinyjs(),
-#   title = "Bond Detals",
-#   nav_panel(
-#     title = "Inputs",
-#     card(
-#       card_header("Bond Details"),
-#       numericInput("face_value", "Face Value", value = 100, step = 1),
-#       numericInput("coupon_rate", "Coupon Rate (Annual)", value = 0.05, min = 0, max = 1, step = 0.01),
-#       selectInput("coupon_freq", "Coupon Frequency", choices = c("Annual","Semi-Annual")),
-#       numericInput("maturity", "Time to Maturity (years)", value = 5, min = 0, max = 30, step = 0.1)
-#     )
-#   )
-# )
-
-
-
-
 library(shiny)
 library(shinydashboard)
 library(bs4Dash)
@@ -331,4 +308,7 @@ ui <- dashboardPage(
           )
         )
       )
+    )
+  )
+)
       
